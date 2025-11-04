8c9170a9b37e44eeacb591ae7933ed8202d27af3
Allenleo1814 <allenleonelperez@gmail.com>
2025-11-04 01:45:59 -0500

    add

---
+++ resources/[cars]/fxmanifest.lua
@@ -0,0 +1,21 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Marlo King Jr'
+description 'GST Vehicle Data'
+version 'Prevail RP 1.0'
+
+files {
+    'cars/**/*.meta',
+    'cars/**/vehicles.meta',
+    'cars/**/carcols.meta',
+    'cars/**/carvariations.meta',
+    'cars/**/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE' 'cars/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'cars/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'cars/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'cars/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'cars/**/vehiclelayouts.meta'
